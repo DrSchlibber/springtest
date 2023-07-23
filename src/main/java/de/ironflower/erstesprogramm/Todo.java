d15a299d1187fddf076b5c610ae058b579668153
nexus <drschlibber@gmail.com>
2023-07-23 07:23:20 +0200

    Todo Klasse

---
+++ src/main/java/de/ironflower/erstesprogramm/Todo.java
@@ -0,0 +1,27 @@
+package de.ironflower.erstesprogramm;
+
+public class Todo {
+	private Integer Id;
+	private String description;
+	private boolean isDone;
+	
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		Id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isDone() {
+		return isDone;
+	}
+	public void setDone(boolean isDone) {
+		this.isDone = isDone;
+	}
+
+}
